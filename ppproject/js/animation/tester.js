f312a49b84c9501fd392fe1048c59d2572101b61
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-02-26 18:29:04 +0200

    Create tester.js

---
+++ ppproject/js/animation/tester.js
@@ -0,0 +1,8 @@
+// Отримуємо кнопку за її класом
+var button = document.querySelector('.your-button-class');
+
+// Додаємо обробник події 'click' для кнопки
+button.addEventListener('click', function() {
+    // Ваш код для обробки події тут
+    console.log('Кнопку натиснули!');
+});
