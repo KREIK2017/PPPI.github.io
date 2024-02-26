c487c1a25db8783155e55c6296978e07a13df7d7
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-02-26 18:56:03 +0200

    Delete ppproject/js/animation/tester.js

--- ppproject/js/animation/tester.js
+++
@@ -1,8 +0,0 @@
-// Отримуємо кнопку за її класом
-var button = document.querySelector('.buterror');
-
-// Додаємо обробник події 'click' для кнопки
-button.addEventListener('click', function() {
-    // Ваш код для обробки події тут
-    console.log('Кнопку натиснули!');
-});
