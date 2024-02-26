d3a1839325d4f72b8830597044255e35b8cb7b2f
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-02-26 18:30:44 +0200

    Update tester.js

--- ppproject/js/animation/tester.js
+++ ppproject/js/animation/tester.js
@@ -1,5 +1,5 @@
 // Отримуємо кнопку за її класом
-var button = document.querySelector('.your-button-class');
+var button = document.querySelector('.buterror');
 
 // Додаємо обробник події 'click' для кнопки
 button.addEventListener('click', function() {
